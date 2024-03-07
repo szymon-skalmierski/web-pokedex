f019226a179ed849f8370ca05d48f26cea4ee699
szymon-skalmierski <xketris@gmail.com>
2024-03-07 21:49:13 +0100

    pokedex components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PokemonListComponent } from './homepage/pokemon-list/pokemon-list.component';
+import { PokemonComponent } from './homepage/pokemon-list/pokemon/pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    PokemonListComponent,
+    PokemonComponent
   ],
   imports: [
     BrowserModule,
